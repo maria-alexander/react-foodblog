f9dc03d27f5e77a92ef2e6a12c117b1870bd608c
Maria Alexander <contact@malexander.dev>
2020-08-09 20:44:40 -0400

    Updates

---
+++ precache-manifest.3c3b5481fd5407d75ce1bb69533cccfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dcfefcf2723dbca19c48c21f319817",
+    "url": "/react-foodblog/index.html"
+  },
+  {
+    "revision": "007460e6d53920d75446",
+    "url": "/react-foodblog/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6f8daae5dac885c2a522",
+    "url": "/react-foodblog/static/css/main.524c07ef.chunk.css"
+  },
+  {
+    "revision": "007460e6d53920d75446",
+    "url": "/react-foodblog/static/js/2.cd9dba4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-foodblog/static/js/2.cd9dba4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8daae5dac885c2a522",
+    "url": "/react-foodblog/static/js/main.46c81e57.chunk.js"
+  },
+  {
+    "revision": "f067acaeccb949637d54",
+    "url": "/react-foodblog/static/js/runtime-main.61241650.js"
+  }
+]);
